99734db9f7e238a61fcbe2deb13c98cb7b4e4728
Marid <maridshikhkerimov@gmail.com>
2021-07-29 09:44:50 +0300

    Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          
+            <Card onClick={(num) => console.log('click', num)} variant={CardVariant.primary} width='200px' height='200px'>
+                <button>Кнопка</button>
+
+            </Card>
         </div>
     );
 };
